7f1a7111cbf7d8d703222a67ff29c4ead6d943e9
Kenneth Anderson <andersonenvy@gmail.com>
2015-08-10 19:15:45 -0400

    Initial Commit of Error Files

---
+++ 403.php
@@ -0,0 +1,9 @@
+<?php
+$body_class = "error" ;
+?>
+<?php include "inc/variables.php"; ?>
+<?php include "inc/header.php"; ?>
+<?php include "inc/nav.php"; ?>
+<h1>Error 403</h1>
+<?php include "inc/error.php"; ?>
+<?php include "inc/footer.php"; ?>
