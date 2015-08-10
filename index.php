ab5975d640cfdff91334180a1775eea3634a3af4
Kenneth Anderson <andersonenvy@gmail.com>
2015-08-10 19:16:41 -0400

    Added Index File

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$body_class = "index" ;
+?>
+<?php include "inc/variables.php"; ?>
+<?php include "inc/header.php"; ?>
+<style type="text/css">
+/* Page Specific CSS */
+</style>
+<script type="text/javascript">
+$(document).ready(function(){
+// ----- Do Stuff ----- //
+/* Page Specific JavaScript */
+// ----- End Document Ready Function ----- //
+});
+</script>
+<?php include "inc/nav.php"; ?>
+<!-- ////////////////////////////////////////////// Begin Page -->
+<h1><a href="http://<?php echo ($site_url) ; ?>"><?php echo ($site_title) ; ?></a></h1>
+<h2><?php echo ($site_description) ; ?></h2>
+<p>Hello World</p>
+<!-- ////////////////////////////////////////////// End Page -->
+<?php include "inc/footer.php"; ?>
