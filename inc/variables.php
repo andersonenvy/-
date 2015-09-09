791eb016b63fb89b581f7184cf570ca5a074fbcf
Kenneth Anderson <andersonenvy@gmail.com>
2015-09-09 12:14:17 -0400

    Use CDN for Font Awesome

--- inc/variables.php
+++ inc/variables.php
@@ -7,4 +7,6 @@ $site_keywords = "andersonenvy, fucking, website";
 $site_url = "www.myfuckingwebsite.com";
 $current_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
+date_default_timezone_set('America/New_York');
+
 ?>
